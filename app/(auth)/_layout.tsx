de7a4c530d4e36d8bfa39235a8e5adaf3f907250
Pipetboy <Felipefelizx@gmail.com>
2025-03-09 01:29:39 -0300

    fix: layout login

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      initialRouteName="login"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: Platform.select({
+          ios: {
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}>
+      <Tabs.Screen
+        name="login"
+        options={{
+          title: 'Login',
+          headerShown: false, 
+        }}
+      />
+    </Tabs>
+  );
+}
