e44984f3bcf8970cc40b7d88baa7193276b909bf
Pipetboy <Felipefelizx@gmail.com>
2025-03-09 17:43:28 -0300

    feat: mejora loyout login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout() {
         {isAuthenticated ? (
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         ) : (
-          <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         )}
       </Stack>
       <StatusBar style="auto" />
